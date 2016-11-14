6b117b9caf1ceb5796c9685066bb02803ff4094b
snowstreams <snowkeeper@snowpi.org>
2016-11-14 01:38:23 -0500

    1.0.4

--- examples/gnutv.js
+++ examples/gnutv.js
@@ -10,31 +10,30 @@ Woobi.init({
 	}
 })
 .then(() => {
-		Woobi.addChannel('TV', {
-			loop: true,
-			// grab the tv stream sent frm another machine to our ip
-			assets: [
-				{
-					type: 'program',
-					name: 'Air',
-					arg: 'gnutv -channels /home/woobi/dvb/channels.conf -out udp 10.10.10.82 13333 WAGA5',
-					redo: 'gnutv -channels /home/woobi/dvb/channels.conf -out udp 10.10.10.82 13333 ##CMD##',
-				},
-				{
-					type: 'udpSink',
-					port: 13333,
-					host: '10.10.10.82',
-					name: 'AirTV',
-				},
-				{
-					type: 'udpStream',
-					port: 13333,
-					host: '10.10.10.87',
-					name: 'streamAir'
-				},
-				
-			],
-		}, (err) => {
-			if(err) console.log('##ERROR##',err);
-		});
+	Woobi.addChannel('TV', {
+		loop: true,
+		assets: [
+			{
+				type: 'program',
+				name: 'Air',
+				arg: 'gnutv -channels /home/woobi/dvb/channels.conf -out udp 10.10.10.82 13333 WAGA5',
+				redo: 'gnutv -channels /home/woobi/dvb/channels.conf -out udp 10.10.10.82 13333 ##CMD##',
+			},
+			{
+				type: 'udpSink',
+				port: 13333,
+				host: '10.10.10.82',
+				name: 'AirTV',
+			},
+			{
+				type: 'udpStream',
+				port: 13333,
+				host: '10.10.10.87',
+				name: 'streamAir'
+			},
+			
+		],
+	}, (err) => {
+		if(err) console.log('##ERROR##',err);
+	});
 });
